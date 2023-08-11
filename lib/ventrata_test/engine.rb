1326ca4341eef96c1c9430d05e363d3f9beeb771
TA Tyree <tatyree@gmail.com>
2023-08-11 15:42:53 +0100

    Configure the engine to use rspec

--- lib/ventrata_test/engine.rb
+++ lib/ventrata_test/engine.rb
@@ -1,6 +1,9 @@
 module VentrataTest
   class Engine < ::Rails::Engine
     isolate_namespace VentrataTest
-    config.generators.api_only = true
+    config.generators do |g|
+      g.api_only = true
+      g.test_framework :rspec
+    end
   end
 end

576fd2e937a996e1642bc263c75c092544cf8b5a
TA Tyree <tatyree@gmail.com>
2023-08-14 10:34:16 +0100

    Set up failing specs for column and Stripe call

--- spec/models/ventrata_test/purchase_spec.rb
+++ spec/models/ventrata_test/purchase_spec.rb
@@ -2,11 +2,25 @@ require 'rails_helper'
 
 module VentrataTest
   RSpec.describe Purchase do
+    let(:valid_attributes) {
+      { amount: 1000, customer: 'bob@example.com', currency: 'USD' }
+    }
+
     it { is_expected.to have_db_column(:amount).of_type(:integer) }
     it { is_expected.to have_db_column(:currency).of_type(:string) }
     it { is_expected.to have_db_column(:customer).of_type(:string) }
+    it { is_expected.to have_db_column(:payment_intent).of_type(:json) }
     it { is_expected.to validate_presence_of(:amount) }
     it { is_expected.to validate_presence_of(:currency) }
     it { is_expected.to validate_presence_of(:customer) }
+
+    context '#get_stripe_payment_intent' do
+      it 'calls Stripe::PaymentIntent.create' do
+        expect(Stripe::PaymentIntent).to receive(:create).with(
+          hash_including(amount: valid_attributes[:amount], currency: valid_attributes[:currency])
+        )
+        described_class.create(valid_attributes)
+      end
+    end
   end
 end
